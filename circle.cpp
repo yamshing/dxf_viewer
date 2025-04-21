675fce74ef51f5ab0b1243a123d483b0acb4b34d
Shingo Yamamoto <yamshing@gmail.com>
2025-04-21 14:37:18 +0900

    redundant setPen removed all test ok

--- circle.cpp
+++ circle.cpp
@@ -8,6 +8,6 @@ void Circle::draw(wxDC& dc) const {
     wxPen pen(Drawable::convertDxfColorToWxColour(color), static_cast<int>(lineWidth)); // Set pen width using lineWidth
     setLineStyle(pen, dc); // Use the new setLineStyle function
     dc.SetBrush(*wxTRANSPARENT_BRUSH);
-    dc.SetPen(pen); // Apply the pen to the device context
+    //dc.SetPen(pen); // Apply the pen to the device context
     dc.DrawCircle(x, y, radius);
 }
--- line.cpp
+++ line.cpp
@@ -4,10 +4,10 @@ Line::Line(int x1, int y1, int x2, int y2, int color, const std::string& lineSty
     : Drawable(x1, y1, color, lineStyle), x1(x1), y1(y1), x2(x2), y2(y2) {}
 
 void Line::draw(wxDC& dc) const {
-    std::cout << "lineWidth: " << lineWidth << std::endl;
+    //std::cout << "lineWidth: " << lineWidth << std::endl;
     wxPen pen(Drawable::convertDxfColorToWxColour(color), static_cast<int>(lineWidth)); // Set pen width using lineWidth
     setLineStyle(pen, dc); // Use the new setLineStyle function
 
-    dc.SetPen(pen); // Apply the pen to the device context
+    //dc.SetPen(pen); // Apply the pen to the device context
     dc.DrawLine(x1, y1, x2, y2);
 }
